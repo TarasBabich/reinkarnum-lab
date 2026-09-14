8b8fd91f7ae99993000d0764dfba53b62cdc7591
TarasBabich <56441856+TarasBabich@users.noreply.github.com>
2026-09-14 16:20:48 +0300

    Add Capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,16 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.reincarnum.lab',
+  appName: 'REINCARNUM',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  },
+  android: {
+    allowMixedContent: false,
+    backgroundColor: '#050807'
+  }
+};
+
+export default config;
